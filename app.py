866e3b65e873b72be6437f81f2b75d4469d604e4
Gops-31 <varun3131997@gmail.com>
2019-02-19 10:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app.secret_key = "secret_key"
 @app.route('/',)
 def func():
    return "hello world"
-
+'''
 if __name__ == '__main__':
   HOST = environ.get('SERVER_HOST', 'localhost')
   try:
       PORT = int(environ.get('SERVER_PORT', '5555'))
   except ValueError:
       PORT = 5555
-     
-  app.run(HOST, port=PORT)
+'''     
+app.run(HOST, port=PORT)
