e31f7b291509f1dcf6b3c73d03966f09a976159d
Gops-31 <varun3131997@gmail.com>
2019-02-25 19:00:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
 from flask import jsonify
 import json
-import pyodbc
+#import pyodbc
 
 app = Flask(__name__)
 
 @app.route('/',)
 def func():
-    cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:tataatsu-server.database.windows.net,1433;Database=tataatsu-database;Uid=tataatsuadmin@tataatsu-server;Pwd=Database@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=200;')
-    cursor = cnxn.cursor()
+    #cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:tataatsu-server.database.windows.net,1433;Database=tataatsu-database;Uid=tataatsuadmin@tataatsu-server;Pwd=Database@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=200;')
+    #cursor = cnxn.cursor()
 
     #res = cursor.execute("SELECT * FROM uni")
     #op = []
