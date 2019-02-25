2d879426e2d37c92ad7dad36f63df0ac6c165b02
Gops-31 <varun3131997@gmail.com>
2019-02-25 18:10:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/',)
 def func():
-    cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:tataatsu-server.database.windows.net,1433;Database=tataatsu-database;Uid=tataatsuadmin@tataatsu-server;Pwd=Database@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+    cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:tataatsu-server.database.windows.net,1433;Database=tataatsu-database;Uid=tataatsuadmin@tataatsu-server;Pwd=Database@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=200;')
     cursor = cnxn.cursor()
 
     #res = cursor.execute("SELECT * FROM uni")
